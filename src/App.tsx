0126fbf2c8f26f8e0de62cdc8e86f6302cc95d53
Sudipto das <sudiptodas877@gmail.com>
2025-09-06 07:26:24 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='w-screen h-screen flex justify-center items-center'>
+   
+    </div>
+    </>
+  )
+}
+
+export default App
